dd653e8c760fc976fefc1a786cc74a1f31e9adc8
Wentrelex <79323963+rainyEra@users.noreply.github.com>
2022-10-29 21:23:44 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <img src="https://wallpaperaccess.com/full/1749181.jpg" alt="asd" />
+    </div>
+  );
+}
